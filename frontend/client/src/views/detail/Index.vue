8c364e96a2476a845235dd0f6d5ae5c23835a5a4
H7x <hqx990809@163.com>
2022-01-11 21:19:04 +0800

    0.1.2

--- frontend/client/src/views/detail/Index.vue
+++ frontend/client/src/views/detail/Index.vue
@@ -1,26 +1,72 @@
 <template>
   <div>
     <van-nav-bar
-      title="标题"
+      title="商店名称"
       left-text="返回"
       left-arrow
       @click-left="onClickLeft"
     />
+    <!-- 商家头部 -->
+    <div class="top">
+      <img src="" alt="" />
+      <span class="storeTitle">XXXXX</span>
+      <span class="introduction">XXXXXXXXXXXXXXX</span>
+    </div>
+    <!-- 商店内容 -->
+    <van-tabs>
+      <van-tab title="菜品" position="left">
+        <van-sidebar v-model="activeKey">
+          <van-sidebar-item title="1" />
+          <van-sidebar-item title="2" />
+          <van-sidebar-item title="3" />
+          <van-sidebar-item title="4" />
+        </van-sidebar>
+        <div>111</div>
+      </van-tab>
+      <van-tab title="评价">评价</van-tab>
+    </van-tabs>
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {};
+    return {
+      activeKey: 0,
+    };
   },
   methods: {
     onClickLeft() {
-      this.$router.push({ path: "/home" });
+      this.$router.go(-1);
     },
   },
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.top {
+  width: 100%;
+  height: 150px;
+  background-color: white;
+  padding-top: 35px;
+  padding-left: 20px;
+  img {
+    display: block;
+    width: 80px;
+    height: 80px;
+    float: left;
+    margin-right: 15px;
+  }
+  .storeTitle {
+    color: #6389fa;
+    font-size: 20px;
+    display: block;
+  }
+  .introduction {
+    font-size: 16px;
+  }
+  .van-tab {
+    display: flex;
+  }
+}
 </style>
--- frontend/client/src/views/login/Index.vue
+++ frontend/client/src/views/login/Index.vue
@@ -1,10 +1,110 @@
 <template>
-  <div>登录页</div>
+  <div>
+    <div class="building">
+      <img src="../../assets/img/logo.png" alt="" />
+      <p>7x么</p>
+      <h1>Welcome</h1>
+      <input
+        type="text"
+        name="phoneNumber"
+        v-model="phoneNumber"
+        class="phoneNumber"
+        placeholder="手机号"
+      />
+      <input
+        type="password"
+        name="password"
+        v-model="password"
+        class="password"
+        placeholder="密码"
+      />
+      <button class="btn">立即登录</button>
+      <p class="text">
+        <router-link to="/registry" class="text1">立即注册</router-link>
+        &nbsp;|&nbsp;
+        <router-link to="/forgot" class="text1">忘记密码?</router-link>
+      </p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      phoneNumber: "",
+      password: "",
+    };
+  },
+};
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+.building {
+  background: url("../../assets/img/login.jpg");
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  background-size: 100% 100%;
+  img {
+    width: 50px;
+    height: 50px;
+    display: block;
+    margin: 100px auto;
+  }
+  p {
+    text-align: center;
+    font-size: 16px;
+    color: gold;
+    margin-top: -100px;
+    margin-bottom: 30px;
+  }
+  h1 {
+    text-align: center;
+    font-size: 50px;
+    font-weight: normal;
+    color: #ecffff;
+  }
+  .phoneNumber {
+    display: block;
+    margin: 100px auto;
+    height: 40px;
+    width: 300px;
+    background-color: transparent;
+    border: 1px solid #ecffff;
+    border-radius: 20px;
+    padding: 0 10px;
+    font-size: 14px;
+  }
+  .password {
+    display: block;
+    margin: -80px auto;
+    height: 40px;
+    width: 300px;
+    background-color: transparent;
+    border: 1px solid #ecffff;
+    border-radius: 20px;
+    padding: 0 10px;
+    font-size: 14px;
+  }
+  .btn {
+    display: block;
+    margin: 100px auto;
+    height: 40px;
+    width: 300px;
+    background-color: transparent;
+    border: 1px solid #ecffff;
+    border-radius: 20px;
+    padding: 0 10px;
+    font-size: 14px;
+    color: #ecffff;
+    letter-spacing: 10px;
+  }
+  .text {
+    margin-top: -90px;
+    text1 {
+      color: #ecffff;
+    }
+  }
+}
 </style>
